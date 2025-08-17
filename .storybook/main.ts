16062acea65976140c965eef746201730ac4cf41
Matheus Rocha Sousa <166670004+Marthplays@users.noreply.github.com>
2025-08-17 00:32:21 -0300

    Finalização do build storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,7 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
+    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/nextjs-vite",
